62996a3a51e3cf19ddb015668e9853b6c7e4ce86
IvanK <mr.koldamasov@mail.ru>
2022-10-21 14:52:37 +0300

    Копия массива с помощью поэлементного копирования

---
+++ Ex045/Program.cs
@@ -0,0 +1,12 @@
+﻿// 45
+
+Console.Clear();
+int[] array = {1, 2, 3, 4, 5};
+int[] b = new int[array.Length];
+for (int i =0; i< array.Length; i++)
+    b[i] = array[i];
+
+b[0] = -10;
+
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Скопированный массив: [{string.Join(", ", b)}]");
