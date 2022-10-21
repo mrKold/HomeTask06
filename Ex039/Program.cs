855dec215b14f3eaa544f542f19419157fdf7e25
IvanK <mr.koldamasov@mail.ru>
2022-10-21 14:17:21 +0300

    Переворот массива

---
+++ Ex039/Program.cs
@@ -0,0 +1,27 @@
+﻿// 39 task
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i <array.Length; i++)
+        array[i] = new Random().Next(0, 10);
+}
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
+Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
